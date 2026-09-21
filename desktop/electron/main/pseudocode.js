298e0404e72e839a7af44eb237ab65907159e8c4
Tizhqy <183872385+Tizhqy@users.noreply.github.com>
2026-09-21 19:13:53 +0300

    .11

--- desktop/electron/main/pseudocode.js
+++ desktop/electron/main/pseudocode.js
@@ -2,16 +2,16 @@
 // Renderer'dan gelen komutlari validate edip Python core bridge'e aktarir.
 
 ipcMain.handle('send-text', async (_event, request) => {
-  validateTextRequest(request);
-  return pythonBridge.sendText(request.text, request.receiverId);
+    validateTextRequest(request);
+    return pythonBridge.sendText(request.text, request.receiverId);
 });
 
 ipcMain.handle('send-pdf', async (_event, request) => {
-  validatePdfPath(request.path);
-  return pythonBridge.sendPdf(request.path, request.receiverId);
+    validatePdfPath(request.path);
+    return pythonBridge.sendPdf(request.path, request.receiverId);
 });
 
 ipcMain.handle('select-profile', async (_event, request) => {
-  validateAllowedProfile(request.profile);
-  return pythonBridge.reloadConfig(request.profile);
+    validateAllowedProfile(request.profile);
+    return pythonBridge.reloadConfig(request.profile);
 });
